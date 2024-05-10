6a60be036bec5c079635008de6694febd3ffa6c7
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-05-10 11:24:07 +0530

    .

--- server/template/contactFormRes.js
+++ server/template/contactFormRes.js
@@ -5,95 +5,108 @@ const contactUsEmail = (
 
 ) => {
     return `<!DOCTYPE html>
-  <html>
-  
-  <head>
-      <meta charset="UTF-8">
-      <title>Contact Form Confirmation</title>
-      <style>
-          body {
-              background-color: #ffffff;
-              font-family: Arial, sans-serif;
-              font-size: 16px;
-              line-height: 1.4;
-              color: #333333;
-              margin: 0;
-              padding: 0;
-          }
-  
-  
-          .container {
-              max-width: 600px;
-              margin: 0 auto;
-              padding: 20px;
-              text-align: center;
-          }
-  
-          .logo {
-              max-width: 200px;
-              margin-bottom: 20px;
-          }
-  
-          .message {
-              font-size: 18px;
-              font-weight: bold;
-              margin-bottom: 20px;
-          }
-  
-          .body {
-              font-size: 16px;
-              margin-bottom: 20px;
-          }
-  
-          .cta {
-              display: inline-block;
-              padding: 10px 20px;
-              background-color: #FFD60A;
-              color: #000000;
-              text-decoration: none;
-              border-radius: 5px;
-              font-size: 16px;
-              font-weight: bold;
-              margin-top: 20px;
-          }
-  
-          .support {
-              font-size: 14px;
-              color: #999999;
-              margin-top: 20px;
-          }
-  
-          .highlight {
-              font-weight: bold;
-          }
-      </style>
-  
-  </head>
-  
-  <body>
-      <div class="container">
-          <a href="http://localhost:5173"><img class="logo"
-                  src="https://i.ibb.co/SVX3TJv/rishi.png" alt="Rishi Maheshwari"></a>
-          <div class="message">New Booking Confimation</div>
-          <div class="body">
-              <p>Dear ${name},</p>
-             
-              <p>Here are the details User provided:</p>
-              <p>Name: ${name}</p>
-              <p>Mobile Number: ${contact}</p>
-              <p>Car Model: ${carType}</p>
-              <p>From : ${from}</p>
-              <p>To: ${to}</p>
-              <p>Pickup Date: ${pickupDate}</p>
-              <p>Drop Date: ${dropDate}</p>
-              <p>Total Distance: ${distance}</p>
-            
-          </div>
-        
-      </div>
-  </body>
-  
-  </html>`
+    <html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Form Confirmation</title>
+        <style>
+            body {
+                background-color: #f4f4f4;
+                font-family: Arial, sans-serif;
+                font-size: 16px;
+                color: #333333;
+                margin: 0;
+                padding: 0;
+            }
+    
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                text-align: center;
+                background-color: #ffffff;
+                border-radius: 10px;
+                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            }
+    
+            .logo {
+                max-width: 200px;
+                margin-bottom: 20px;
+            }
+    
+            .message {
+                font-size: 24px;
+                font-weight: bold;
+                margin-bottom: 20px;
+                color: #333333;
+            }
+    
+            .body {
+                font-size: 18px;
+                margin-bottom: 20px;
+                text-align: left;
+                color: #666666;
+            }
+    
+            .info {
+                margin-bottom: 10px;
+            }
+    
+            .info p {
+                margin: 5px 0;
+            }
+    
+            .highlight {
+                font-weight: bold;
+                color: #FFA500;
+            }
+    
+            .cta {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #FFD60A;
+                color: #000000;
+                text-decoration: none;
+                border-radius: 5px;
+                font-size: 16px;
+                font-weight: bold;
+                margin-top: 20px;
+            }
+    
+            .support {
+                font-size: 14px;
+                color: #999999;
+                margin-top: 20px;
+            }
+        </style>
+    
+    </head>
+    
+    <body>
+        <div class="container">
+            <a href="http://localhost:5173"><img class="logo"
+                    src="https://i.ibb.co/yVxsTbM/pngwing-com-3.png" alt="Car Book"></a>
+            <div class="message">New Booking Confirmation</div>
+            <div class="body">
+                <p>Dear <span class="highlight">Admin</span>,</p>
+                <div class="info">
+                    <p>A new booking has been made::</p>
+                    <p><span class="highlight">Name:</span> ${name}</p>
+                    <p><span class="highlight">Mobile Number:</span> ${contact}</p>
+                    <p><span class="highlight">Car Model:</span> ${carType}</p>
+                    <p><span class="highlight">From:</span> ${from}</p>
+                    <p><span class="highlight">To:</span> ${to}</p>
+                    <p><span class="highlight">Pickup Date:</span> ${pickupDate}</p>
+                    <p><span class="highlight">Drop Date:</span> ${dropDate}</p>
+                    <p><span class="highlight">Total Distance:</span> ${distance}</p>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+    </html>
+    `
 }
 
 
