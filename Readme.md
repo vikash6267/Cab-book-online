4d6beb2d52cd307605f5a4e05b8d4e803cfe8e3c
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-05-10 10:49:47 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Cab Book Online </br>
+ <!-- <div align='center'>
+    <a href="https://movie-app-tasks.netlify.app"> 🌐 Live 🌐</a> </div> </br> -->
+
+# Tech Stack Used
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![NPM](https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&logo=npm&logoColor=white)
+![Netlify](https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7)
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+
+# Overview  </br>
+ ## A login-free MERN car rental project simplifies development, enabling users to browse cars and make bookings anonymously. It streamlines backend and frontend tasks, omitting authentication-related complexities. While personalized features are limited, it offers a frictionless user experience
+
+
+# Setup And Testing
+
+# Frontend 
+npm install
+npm run dev
+
+# Backend
+npm install
+npm run dev
---
+++ client/src/components/BookCar.jsx
@@ -0,0 +1,292 @@
+import { useEffect, useState } from "react";
+import { IconCar, IconInfoCircleFilled, IconX } from "@tabler/icons-react";
+import { IconMapPinFilled } from "@tabler/icons-react";
+import { IconCalendarEvent } from "@tabler/icons-react";
+import { IconUser } from "@tabler/icons-react";
+import { IconPhone } from "@tabler/icons-react";
+
+
+function BookCar() {
+  // booking car
+  const [carType, setCarType] = useState("");
+  const [pickTime, setPickTime] = useState("");
+  const [dropTime, setDropTime] = useState("");
+  const [carImg, setCarImg] = useState("");
+const[name,setName] = useState("")
+const[contact,setContact] = useState("")
+
+
+
+  const [fromValue, setFromValue] = useState('');
+  const [toValue, setToValue] = useState('');
+  const [fromSuggestions, setFromSuggestions] = useState([]);
+  const [toSuggestions, setToSuggestions] = useState([]);
+  const [totalDistance, setTotalDistance] = useState(null);
+
+  const onChangeFrom = (event) => {
+    const value = event.target.value;
+    setFromValue(value);
+    fetchSuggestions(value, setFromSuggestions);
+  };
+
+  const onChangeTo = (event) => {
+    const value = event.target.value;
+    setToValue(value);
+    fetchSuggestions(value, setToSuggestions);
+  };
+
+  const fetchSuggestions = (value, setSuggestions) => {
+    const service = new window.google.maps.places.AutocompleteService();
+    service.getPlacePredictions({ input: value }, (predictions) => {
+      setSuggestions(predictions || []);
+    });
+  };
+
+  const onSuggestionSelectedFrom = (suggestion) => {
+    setFromValue(suggestion.description);
+    setFromSuggestions([]);
+  };
+
+  const onSuggestionSelectedTo = (suggestion) => {
+    setToValue(suggestion.description);
+    setToSuggestions([]);
+  };
+
+
+
+
+
+
+  // taking value of booking inputs
+  const handleCar = (e) => {
+    setCarType(e.target.value);
+    setCarImg(e.target.value);
+  };
+
+
+
+  const handlePickTime = (e) => {
+    setPickTime(e.target.value);
+  };
+
+  const handleDropTime = (e) => {
+    setDropTime(e.target.value);
+  };
+
+  // based on value name show car img
+
+
+  // hide message
+  const hideMessage = () => {
+    const doneMsg = document.querySelector(".booking-done");
+    doneMsg.style.display = "none";
+  };
+
+  const submitData =(e) =>{
+    e.preventDefault()
+    console.log("hello")
+
+    let formData = new FormData()
+
+    formData.append("name" , name)
+    formData.append("carType",carType)
+    formData.append("from",fromValue)
+    formData.append("to",toValue)
+    formData.append("pickupDate",pickTime)
+    formData.append("dropDate",dropTime)
+    formData.append("contact",contact)
+    formData.append("distance",totalDistance)
+
+    console.log(formData)
+
+
+  }
+
+  return (
+    <>
+      <section id="booking-section" className="book-section">
+        {/* overlay */}
+       
+
+        <div className="container">
+          <div className="book-content">
+            <div className="book-content__box">
+              <h2>Book a car</h2>
+
+              {/* <p className="error-message">
+                All fields required! <IconX width={20} height={20} />
+              </p>
+
+              <p className="booking-done">
+                Check your email to confirm an order.{" "}
+                <IconX width={20} height={20} onClick={hideMessage} />
+              </p> */}
+
+              <form className="box-form" onSubmit={submitData}>
+
+              <div className="box-form__car-type">
+                  <label>
+                    <IconUser className="input-icon" /> &nbsp; Enter Your Name <b>*</b>
+                  </label>
+               <input type="text" 
+               placeholder="Enter Your name"
+               value={name}
+               onChange={(e)=>setName(e.target.value)}
+          className="w-full px-4 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-2xl"
+               
+                />
+                </div>
+
+
+
+                <div className="box-form__car-type">
+                  <label>
+                    <IconPhone className="input-icon" /> &nbsp; Contact Number <b>*</b>
+                  </label>
+               <input type="text" 
+               placeholder="Enter Your Number"
+               value={contact}
+               onChange={(e)=>setContact(e.target.value)}
+          className="w-full px-4 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-2xl"
+               
+                />
+                </div>
+
+
+
+
+
+                <div className="box-form__car-type">
+                  <label>
+                    <IconCar className="input-icon" /> &nbsp; Select Your Car
+                    Type <b>*</b>
+                  </label>
+                  <select value={carType} onChange={handleCar}>
+                    <option>Select your car type</option>
+                    <option value="Audi A1 S-Line">Audi A1 S-Line</option>
+                    <option value="VW Golf 6">VW Golf 6</option>
+                    <option value="Toyota Camry">Toyota Camry</option>
+                    <option value="BMW 320 ModernLine">
+                      BMW 320 ModernLine
+                    </option>
+                    <option value="Mercedes-Benz GLK">Mercedes-Benz GLK</option>
+                    <option value="VW Passat CC">VW Passat CC</option>
+                  </select>
+                </div>
+
+                <div className="box-form__car-type">
+                  <label>
+                    <IconMapPinFilled className="input-icon" /> &nbsp; Pick-up{" "}
+                    <b>*</b>
+                  </label>
+                  {/* <select value={pickUp} onChange={handlePick}>
+                    <option>Select pick up location</option>
+                    <option>Belgrade</option>
+                    <option>Novi Sad</option>
+                    <option>Nis</option>
+                    <option>Kragujevac</option>
+                    <option>Subotica</option>
+                  </select> */}
+
+                  <input
+          type="text"
+          placeholder="From"
+          value={fromValue}
+          onChange={onChangeFrom}
+          className="w-full px-4 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-2xl"
+        />
+        <ul className="mt-2">
+          {fromSuggestions.map((suggestion) => (
+            <li
+              key={suggestion.place_id}
+              onClick={() => onSuggestionSelectedFrom(suggestion)}
+              className="cursor-pointer hover:bg-gray-100 px-4 py-2 text-2xl"
+            >
+              {suggestion.description}
+            </li>
+          ))}
+        </ul>
+
+
+                </div>
+
+                <div className="box-form__car-type">
+                  <label>
+                    <IconMapPinFilled className="input-icon" /> &nbsp; Drop-of{" "}
+                    <b>*</b>
+                  </label>
+                  {/* <select value={dropOff} onChange={handleDrop}>
+                    <option>Select drop off location</option>
+                    <option>Novi Sad</option>
+                    <option>Belgrade</option>
+                    <option>Nis</option>
+                    <option>Kragujevac</option>
+                    <option>Subotica</option>
+                  </select> */}
+
+                  <input
+          type="text"
+          placeholder="To"
+          value={toValue}
+          onChange={onChangeTo}
+          className="w-full px-4 py-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-2xl"
+        />
+        <ul className="mt-2">
+          {toSuggestions.map((suggestion) => (
+            <li
+              key={suggestion.place_id}
+              onClick={() => onSuggestionSelectedTo(suggestion)}
+              className="cursor-pointer hover:bg-gray-100 px-4 py-2 text-2xl"
+            >
+              {suggestion.description}
+            </li>
+          ))}
+        </ul>
+
+                </div>
+
+                <div className="box-form__car-time ">
+                  <label htmlFor="picktime" className="flex">
+                    <IconCalendarEvent className="input-icon" /> &nbsp; Pick-up{" "}
+                    <b>*</b>
+                  </label>
+                  <input
+                    id="picktime"
+                    value={pickTime}
+                    onChange={handlePickTime}
+                    type="date"
+                    min={new Date().toISOString().split('T')[0]}
+                  ></input>
+                </div>
+
+                <div className="box-form__car-time">
+                  <label htmlFor="droptime" className=" flex ">
+                    <IconCalendarEvent className="input-icon" /> &nbsp; Drop-of{" "}
+                    <b>*</b>
+                  </label>
+                  <input
+                    id="droptime"
+                    value={dropTime}
+                    onChange={handleDropTime}
+                    type="date"
+                    min={pickTime}
+                  ></input>
+                </div>
+
+                <button  type="submit">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* modal ------------------------------------ */}
+
+     
+    </>
+  );
+}
+
+export default BookCar;
